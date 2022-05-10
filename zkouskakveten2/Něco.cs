99effcb28178d208a9f27e39e9b142dfe31a9293
Jan Gergel <jan.gergel@educhem.cz>
2022-05-10 09:05:12 +0200

    Přidejte soubory projektu.

---
+++ zkouskakveten2/Něco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zkouskakveten2
+{
+    internal class Něco
+    {
+        public int jídlo { get; set; }
+        public int zvíře { get; set; }
+        public int Uživatel { get; set; }
+        
+        public Něco()
+        {
+            Console.WriteLine("Objekt vytvořen");
+        }
+        public void info()
+        {
+            Console.WriteLine()
+        }
+    }
+}
